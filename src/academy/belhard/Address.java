67f3ef65de92463c5df23495617dd95384c2db95
MaxRyzhykh <72518453+MaxRyzhykh@users.noreply.github.com>
2020-11-11 17:27:43 +0300

    first commit

---
+++ src/academy/belhard/Address.java
@@ -0,0 +1,15 @@
+package academy.belhard;
+
+public class Address {
+    private String city;
+    private String street;
+    private int houseNumb;
+
+    public Address(String city, String street, int houseNumb) {
+        this.city = city;
+        this.street = street;
+        this.houseNumb = houseNumb;
+    }
+
+    h
+}
